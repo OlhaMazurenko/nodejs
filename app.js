cc1bd990a8570a3db05687ab4bb2337392a496aa
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2020-06-18 01:24:05 +0400

    Adding products model

--- app.js
+++ app.js
@@ -23,9 +23,8 @@ app.use(errorController.get404);
 
 sequelize.sync()
     .then(result => {
-        console.log(result);
-    app.listen(3000);
-
+        // console.log(result);
+        app.listen(3000);
     })
     .catch(err => {
         console.log(err);
