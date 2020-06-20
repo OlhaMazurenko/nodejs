e4b065c6a0b745f96e460f6a00db803d5096652e
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2020-06-21 02:16:40 +0400

    one to many

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'SuzasaHehataEdifier', 
+const sequelize = new Sequelize('node-complete', 'root', '***', 
     {
         dialect: 'mysql', host: 'localhost'
     }
