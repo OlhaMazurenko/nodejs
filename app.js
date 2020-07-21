a22c73fbb29f9ed9e3a8e4c8192e18f6ff28425f
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2020-07-21 22:42:42 +0300

    Cart

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -26,7 +28,8 @@ app.use((req, res, next) => {
     next();
   })
   .catch(err => console.log(err));
-})
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -34,12 +37,15 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
 
 sequelize
   // .sync({ force: true })
   .sync()
   .then(result => {
-    // console.log(result);
     return User.findByPk(1);
     // app.listen(3001);
   })
@@ -50,7 +56,9 @@ sequelize
     return user;
   })
   .then(user => {
-    console.log("ggggggggg", user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3001);
   })
   .catch(err => {
--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'SuzasaHehataEdifier', 
+const sequelize = new Sequelize('node_complete', 'root', 'root', 
     {
         dialect: 'mysql', host: 'localhost'
     }
